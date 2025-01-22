b0d2d82b5d4c3573f295fb1699db56237f57ac41
Diggo Silva <rdiggosilva@gmail.com>
2025-01-22 10:49:29 -0300

    Feature/main screen (#1)
    * Created main screen with clickable and animated logo to launch the app
    
    * Created Extensions file
    
    * Also added SD WebImage lib from cocoapods

---
+++ GuiaBolso-Desafio-iOS/App/Utils/Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions.swift
+//  GuiaBolso-Desafio-iOS
+//
+//  Created by Diggo Silva on 22/01/25.
+//
+
+import UIKit
+import SDWebImage
+
+extension UIImageView {
+    static let chuckNorrisImageUrl = "https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png"
+    
+    func setChuckNorrisImage() {
+        guard let url = URL(string: UIImageView.chuckNorrisImageUrl) else {
+            print("URL inválida")
+            return
+        }
+        self.sd_setImage(with: url)
+    }
+}
