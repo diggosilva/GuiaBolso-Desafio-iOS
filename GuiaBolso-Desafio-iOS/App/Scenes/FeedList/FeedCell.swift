c926822f4e8758ff01a5ffb2a2b8b1e02d9d3e3b
Diggo Silva <rdiggosilva@gmail.com>
2025-01-23 09:47:41 -0300

    Created custom cell

--- GuiaBolso-Desafio-iOS/App/Scenes/FeedList/FeedCell.swift
+++ GuiaBolso-Desafio-iOS/App/Scenes/FeedList/FeedCell.swift
@@ -5,4 +5,43 @@
 //  Created by Diggo Silva on 22/01/25.
 //
 
-import Foundation
+import UIKit
+
+class FeedCell: UITableViewCell {
+    static let identifier = "FeedCell"
+    
+    lazy var categoryLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    func configure(model: Model) {
+        categoryLabel.text = model.category.capitalized
+        self.accessoryType = .disclosureIndicator
+    }
+    
+    private func setHierarchy() {
+        addSubview(categoryLabel)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            categoryLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            categoryLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            categoryLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+        ])
+    }
+}
