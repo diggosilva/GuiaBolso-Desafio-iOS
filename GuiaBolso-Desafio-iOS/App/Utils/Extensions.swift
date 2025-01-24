4fa109e9e681e858c089bcece48a7e44e447547e
Diggo Silva <rdiggosilva@gmail.com>
2025-01-24 09:15:01 -0300

    Implemented method applyShadow()

--- GuiaBolso-Desafio-iOS/App/Utils/Extensions.swift
+++ GuiaBolso-Desafio-iOS/App/Utils/Extensions.swift
@@ -24,4 +24,11 @@ extension UIView {
     func addSubviews(_ views: [UIView]) {
         views.forEach({ addSubview($0) })
     }
+    
+    func applyShadow(view: UIView) {
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: 5, height: 5)
+        view.layer.shadowOpacity = 0.75
+        view.layer.shadowRadius = 5.0
+    }
 }
