969d0b15b019b68bc2d8968a35b2dd1681a432f4
Diggo Silva <rdiggosilva@gmail.com>
2025-01-24 09:09:00 -0300

    Created object JokeResponse

--- GuiaBolso-Desafio-iOS/App/Model/JokeResponse.swift
+++ GuiaBolso-Desafio-iOS/App/Model/JokeResponse.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+// MARK: - JokeResponse
+struct JokeResponse: Codable {
+    let categories: [String]
+    let createdAt: String
+    let iconURL: String
+    let id, updatedAt: String
+    let url: String
+    let value: String
+    
+    enum CodingKeys: String, CodingKey {
+        case categories
+        case createdAt = "created_at"
+        case iconURL = "icon_url"
+        case id
+        case updatedAt = "updated_at"
+        case url, value
+    }
+}
