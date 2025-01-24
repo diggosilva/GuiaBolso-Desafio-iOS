4cae407e8b753267038d59304d7cb2f18dadcb04
Diggo Silva <rdiggosilva@gmail.com>
2025-01-24 09:15:59 -0300

    Updated

--- GuiaBolso-Desafio-iOS/App/Scenes/Joke/JokeViewController.swift
+++ GuiaBolso-Desafio-iOS/App/Scenes/Joke/JokeViewController.swift
@@ -5,4 +5,66 @@
 //  Created by Diggo Silva on 23/01/25.
 //
 
-import Foundation
+import UIKit
+
+class JokeViewController: UIViewController {
+    
+    let jokeView = JokeView()
+    let viewModel: JokeViewModel
+    
+    init(categoty: String) {
+        self.viewModel = JokeViewModel(category: categoty)
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    override func loadView() {
+        super.loadView()
+        view = jokeView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        handleStates()
+        viewModel.loadJoke()
+    }
+    
+    private func handleStates() {
+        viewModel.state.bind { state in
+            switch state {
+            case .loading:
+                return self.showLoadingState()
+            case .loaded(let jokeModel):
+                return self.showLoadedState(jokeModel: jokeModel)
+            case .error:
+                return self.showErrorState()
+            }
+        }
+    }
+    
+    private func showLoadingState() {
+        jokeView.spinner.startAnimating()
+        jokeView.loadingLabel.isHidden = false
+    }
+    
+    private func showLoadedState(jokeModel: JokeModel) {
+        jokeView.setAlphaView(duration: 0.25, alpha: 1)
+        jokeView.configure(model: jokeModel)
+        jokeView.spinner.stopAnimating()
+        jokeView.loadingLabel.isHidden = true
+    }
+    
+    private func showErrorState() {
+        let alert = UIAlertController(title: "Acredite se quiser!", message: "Chuck Norris está com problemas. 🤣", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Entendi", style: .default) { action in
+            self.jokeView.bgJoke.isHidden = true
+            self.jokeView.fgJoke.isHidden = true
+            self.jokeView.spinner.stopAnimating()
+            self.jokeView.loadingLabel.text = "Tente mais tarde! 🥲"
+        }
+        
+        alert.addAction(ok)
+        present(alert, animated: true)
+    }
+}
